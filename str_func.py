a2c72cfea05ac8b1cfc929e6fa6ce70688bab912
Zotov <ZotovDendos@yandex.ru>
2023-05-11 22:08:39 +0300

    added title function in str_func.py and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_string(string):
+    """Функция делает все буквы заглавными"""
     return string.upper()
+
+
+def title_string(string):
+    """Функция делает первый символ каждого слова в строке заглавным"""
+    return string.title()
